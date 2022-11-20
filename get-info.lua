275347b862bec89f1733fbde3f516e4de6ce3eba
garret <garret@airmail.cc>
2022-11-20 02:57:52 +0000

    info getter: add config support

--- get-info.lua
+++ get-info.lua
@@ -3,6 +3,8 @@ local inspect = require "inspect"
 print(arg[1])
 local run_macro = loadfile(arg[1])
 
+local conf = loadfile("conf.lua")()
+
 run_macro()
 
 print(script_name)
@@ -11,4 +13,5 @@ print(script_author)
 print(script_version)
 print(script_namespace)
 print(inspect(__feedmaker_version))
+print(inspect(conf))
 --print(inspect(_G))
