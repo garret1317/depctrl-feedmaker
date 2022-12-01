89e494b46fbe120e012f4a13cfffc5f85cd9ad89
garret <garret@airmail.cc>
2022-12-01 00:06:20 +0000

    add deepcopy from lua-users

--- make-feed.lua
+++ make-feed.lua
@@ -105,6 +105,27 @@ local function get_file_metadata(file)
 	return hash, lastmodified
 end
 
+local function deepcopy(orig, copies) -- copied and pasted from https://lua-users.org/wiki/CopyTable
+	copies = copies or {}
+	local orig_type = type(orig)
+	local copy
+	if orig_type == 'table' then
+		if copies[orig] then
+			copy = copies[orig]
+		else
+			copy = {}
+			copies[orig] = copy
+			for orig_key, orig_value in next, orig, nil do
+				copy[deepcopy(orig_key, copies)] = deepcopy(orig_value, copies)
+			end
+			setmetatable(copy, deepcopy(getmetatable(orig), copies))
+		end
+	else -- number, string, boolean, etc
+		copy = orig
+	end
+	return copy
+end
+
 local noop = function() end
 
 local function fake_depctrl(i)
